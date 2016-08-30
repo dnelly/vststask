814de28d7f34a03c4d7163718922765624b060af
Darnell McMurrian <dlmcmurrian@gmail.com>
2016-08-30 11:27:49 -0400

    removed the my from all the variables.

--- src/tasks/WebJobsTools/WebJobUtility.ps1
+++ src/tasks/WebJobsTools/WebJobUtility.ps1
@@ -13,12 +13,12 @@ try {
     # # Output the message to the log.
     # Write-Host (Get-VstsInput -Name msg)
 
-    [string]$MyJobName = Get-VstsInput -Name JobName -Require
+    [string]$JobName = Get-VstsInput -Name JobName -Require
     [string]$AzureWebAppName = Get-VstsInput -Name AzureWebAppName -Require
     # [bool]$StartJob = Get-VstsInput -Name StartJob
     # [bool]$StopJob = Get-VstsInput -Name StopJob
     [string]$JobState = Get-VstsInput -Name JobState
-    [string]$MyJobType = Get-VstsInput -Name JobType -Require
+    [string]$JobType = Get-VstsInput -Name JobType -Require
     [string]$UserName = Get-VstsInput -Name UserName -Require
     [string]$Password = Get-VstsInput -Name Password -Require
     $token =  [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($UserName):$($Password)"));
@@ -26,7 +26,7 @@ try {
     $headers.Add("Authorization", [string]::Format('Basic {0}',$token))
     $requestUribase = 'https://{0}.scm.azurewebsites.net/api/{1}webjobs/{2}/{3}'
 
-    Write-Host "[JobName] -> $Jobname"
+    Write-Host "[JobName] -> $JobName"
     Write-Host "[AzureWebAppName] -> $AzureWebAppName"
     Write-Host "[JobState] -> $JobState"
     Write-Host "[JobType] -> $JobType"
@@ -39,9 +39,9 @@ try {
         exit 0
     }
 
-    Write-Host "$JobState Job $MyJobName"
-    $postRequest = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,$JobState.ToLower())
-    $getRequest = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,"")
+    Write-Host "$JobState Job $JobName"
+    $postRequest = [string]::Format($requestUribase, $AzureWebAppName, $JobType, $JobName,$JobState.ToLower())
+    $getRequest = [string]::Format($requestUribase, $AzureWebAppName, $JobType, $JobName,"")
 
     Write-host "Processing current request for URI"
     Write-Host $postRequest
