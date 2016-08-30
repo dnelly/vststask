39cbae3c8e309f2679e16b63907f66204fbeaf79
Darnell McMurrian <dlmcmurrian@gmail.com>
2016-08-30 11:16:18 -0400

    added logging and a step to get the status of the job.

--- src/tasks/WebJobsTools/WebJobUtility.ps1
+++ src/tasks/WebJobsTools/WebJobUtility.ps1
@@ -21,16 +21,26 @@ try {
     [string]$MyJobType = Get-VstsInput -Name JobType -Require
     [string]$UserName = Get-VstsInput -Name UserName -Require
     [string]$Password = Get-VstsInput -Name Password -Require
-    assert-
     $token =  [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($UserName):$($Password)"));
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
     $headers.Add("Authorization", [string]::Format('Basic {0}',$token))
     $requestUribase = 'https://{0}.scm.azurewebsites.net/api/{1}webjobs/{2}/{3}'
 
+    Write-Host "[JobName] -> $Jobname"
+    Write-Host "[AzureWebAppName] -> $AzureWebAppName"
+    Write-Host "[JobState] -> $JobState"
+    Write-Host "[JobType] -> $JobType"
+    Write-Host "[UserName] -> $UserName"
 
     Write-Host "$JobState Job $MyJobName"
-    $request = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,$JobState.ToLower())
-    $response = Invoke-RestMethod $request -Headers $headers -ContentType 'application/json'
+    $postRequest = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,$JobState.ToLower())
+    $getRequest = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,"")
+
+    Write-host "Processing current request for URI"
+    Write-Host $postRequest
+    $response = Invoke-RestMethod $postRequest -Headers $headers -ContentType 'application/json' -Method Post
+    Write-Host "Getting the status of $Jobname"
+    Invoke-RestMethod $getrequest.Trim() -Headers $headers -ContentType 'application/json' -Method Get
 
 } finally {
     Trace-VstsLeavingInvocation $MyInvocation
