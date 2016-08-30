67b8d736ede5fd369e94f0947f73dcd6a7b13b5f
Darnell McMurrian <dlmcmurrian@gmail.com>
2016-08-30 10:38:12 -0400

    updates and added modules.

--- src/tasks/WebJobsTools/WebJobUtility.ps1
+++ src/tasks/WebJobsTools/WebJobUtility.ps1
@@ -1,5 +1,5 @@
 param(
-    # Web Job Name
+<#    # Web Job Name
     [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'StartJob')]
     [Parameter(Mandatory = $true, ParameterSetName = 'StopJob')]
     [string]
@@ -37,26 +37,54 @@ param(
     [Parameter(Mandatory = $true, ParameterSetName = 'StartJob')]
     [Parameter(Mandatory = $true, ParameterSetName = 'StopJob')]
     [string]
-    $Password
+    $Password#>
 
 
 
 )
 
 
-$token =  [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($UserName):$($Password)"));
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", [string]::Format('Basic {0}',$token))
-$requestUribase = 'https://{0}.scm.azurewebsites.net/api/{1}webjobs/{2}/{3}'
 
 
-if ($StartJob) {
-    Write-Host "Starting Job $MyJobName"
-    $request = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,"start")
+Trace-VstsEnteringInvocation $MyInvocation
+try {
+    Import-VstsLocStrings "$PSScriptRoot\Task.json"
+    
+    # Set the working directory.
+<#    $cwd = Get-VstsInput -Name cwd -Require
+    Assert-VstsPath -LiteralPath $cwd -PathType Container
+    Write-Verbose "Setting working directory to '$cwd'."
+    Set-Location $cwd
+
+    # Output the message to the log.
+    Write-Host (Get-VstsInput -Name msg)#>
+
+    [string]$MyJobName = Get-VstsInput -Name JobName -Require
+    [string]$AzureWebAppName = Get-VstsInput -Name AzureWebAppName -Require
+    [bool]$StartJob = Get-VstsInput -Name StartJob
+    [bool]$StopJob = Get-VstsInput -Name StopJob
+    [string]$JobState = Get-VstsInput -Name JobState
+    [string]$MyJobType = Get-VstsInput -Name JobType -Require
+    [string]$UserName = Get-VstsInput -Name UserName -Require
+    [string]$Password = Get-VstsInput -Name Password -Require
+    assert-
+    $token =  [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($UserName):$($Password)"));
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", [string]::Format('Basic {0}',$token))
+    $requestUribase = 'https://{0}.scm.azurewebsites.net/api/{1}webjobs/{2}/{3}'
+
+
+<#    if ($JobState -eq "Start") {
+        Write-Host "Starting Job $MyJobName"        
+    }
+    elseif ($JobState -eq "Stop") {
+        Write-Host "Stopping Job $MyJobName"
+        # $request = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,"stop")
+    }#>
+    Write-Host "$JobState Job $MyJobName"
+    $request = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,$JobState.ToLower())
+    $response = Invoke-RestMethod $request -Headers $headers -ContentType 'application/json'
+
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
 }
-elseif ($StopJob) {
-    Write-Host "Stopping Job $MyJobName"
-    $request = [string]::Format($requestUribase, $AzureWebAppName, $MyJobType, $MyJobName,"stop")
-}
-
-$response = Invoke-RestMethod $request -Headers $headers -ContentType 'application/json'
