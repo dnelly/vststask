c08505818134ac1cc74d094825a89a51615110a5
Darnell McMurrian <dlmcmurrian@gmail.com>
2016-09-09 10:31:03 -0400

    more changes

--- src/tasks/AzureWebAppSettingsUpdater/UpdateAzureRMWebAppSetings.ps1
+++ src/tasks/AzureWebAppSettingsUpdater/UpdateAzureRMWebAppSetings.ps1
@@ -7,110 +7,110 @@ Write-Host "Starting Updates."
 # https://github.com/Microsoft/vsts-task-lib
 Trace-VstsEnteringInvocation $MyInvocation
 
-function GetAppSettings ([xml]$configFile) {
-    $tempappsettings = @{}
-    write-host "Starting Appsettings"
-    echo $configFile.configuration.appSettings.add
-    foreach($setting in $configFile.configuration.appSettings.add)
-    {
-        $tempappsettings.Add($setting.key,$setting.value)
-    }
+# function GetAppSettings ([xml]$configFile) {
+#     $tempappsettings = @{}
+#     write-host "Starting Appsettings"
+#     echo $configFile.configuration.appSettings.add
+#     foreach($setting in $configFile.configuration.appSettings.add)
+#     {
+#         $tempappsettings.Add($setting.key,$setting.value)
+#     }
 
-    write-host "Finished Appsettings"
+#     write-host "Finished Appsettings"
 
-    return [hashtable]$tempappsettings
-}
+#     return [hashtable]$tempappsettings
+# }
 
-function GetAppSettingNames ([xml]$configFile) {
-    $tempappsettingsNames = @()
+# function GetAppSettingNames ([xml]$configFile) {
+#     $tempappsettingsNames = @()
 
-    write-host "Getting Appsettings Names"
-    echo $configFile.configuration.appSettings.add
-    foreach($setting in $configFile.configuration.appSettings.add)
-    {
-        $tempappsettingsNames =$tempappsettingsNames + $setting.key
-    }
+#     write-host "Getting Appsettings Names"
+#     echo $configFile.configuration.appSettings.add
+#     foreach($setting in $configFile.configuration.appSettings.add)
+#     {
+#         $tempappsettingsNames =$tempappsettingsNames + $setting.key
+#     }
 
-    write-host "Finished Getting Appsettings Names"
+#     write-host "Finished Getting Appsettings Names"
     
-    return $tempappsettingsNames
-}
+#     return $tempappsettingsNames
+# }
 
-function GetConnectionStrings ([xml]$configFile) {
-    $tempconnections = @{}
-    write-host "Starting Conectionstrings"
-    echo $configFile.configuration.connectionStrings.add
-    foreach($setting in $configFile.configuration.connectionStrings.add)
-    {        
-        if ($UseSQLServerType -eq "true") {
-            if ($SQLServerConnectionNames.split(";").split(",").contains($setting.name)) {
-                $connectionType = "SqlServer"
-            }
-        }
-        elseif ($UseAzureSQLType -eq "true") {
-            if ($AzureSQLConnectionNames.split(";").split(",").contains($setting.name)) {
-                $connectionType = "AzureSQL"
-            }            
+# function GetConnectionStrings ([xml]$configFile) {
+#     $tempconnections = @{}
+#     write-host "Starting Conectionstrings"
+#     echo $configFile.configuration.connectionStrings.add
+#     foreach($setting in $configFile.configuration.connectionStrings.add)
+#     {        
+#         if ($UseSQLServerType -eq "true") {
+#             if ($SQLServerConnectionNames.split(";").split(",").contains($setting.name)) {
+#                 $connectionType = "SqlServer"
+#             }
+#         }
+#         elseif ($UseAzureSQLType -eq "true") {
+#             if ($AzureSQLConnectionNames.split(";").split(",").contains($setting.name)) {
+#                 $connectionType = "AzureSQL"
+#             }            
             
-        }
-        elseif ($UseMySqlType -eq "true") {
-            if ($MySQLConnectionNames.split(";").split(",").contains($setting.name)) {
-                $connectionType = "MySql"
-            }
+#         }
+#         elseif ($UseMySqlType -eq "true") {
+#             if ($MySQLConnectionNames.split(";").split(",").contains($setting.name)) {
+#                 $connectionType = "MySql"
+#             }
             
-        }
-        elseif ($UseCustomType -eq "true") {
-            if ($CustomTypeConnectionNames.split(";").split(",").contains($setting.name)) {
-                $connectionType = "Custom"
-            }            
+#         }
+#         elseif ($UseCustomType -eq "true") {
+#             if ($CustomTypeConnectionNames.split(";").split(",").contains($setting.name)) {
+#                 $connectionType = "Custom"
+#             }            
             
-        }
+#         }
 
-        $tempconnections[$setting.name] = @{Type = $connectionType;Value = $setting.connectionString}
-    }
-    write-host "Finished Parsing ConnectionString"
-    return [hashtable]$tempconnections
-}
+#         $tempconnections[$setting.name] = @{Type = $connectionType;Value = $setting.connectionString}
+#     }
+#     write-host "Finished Parsing ConnectionString"
+#     return [hashtable]$tempconnections
+# }
 
-function GetConnectionStringNames ([xml]$configFile) {
-    $tempconnectionNames = @()
-    write-host "Getting Conectionstrings Names"
-    echo $configFile.configuration.connectionStrings.add
-    foreach($setting in $configFile.configuration.connectionStrings.add)
-    {        
-        $tempconnectionNames = $tempconnectionNames + $setting.name
-    }
-    write-host "Finished Getting ConnectionString Names"
-    return $tempconnectionNames
-}
+# function GetConnectionStringNames ([xml]$configFile) {
+#     $tempconnectionNames = @()
+#     write-host "Getting Conectionstrings Names"
+#     echo $configFile.configuration.connectionStrings.add
+#     foreach($setting in $configFile.configuration.connectionStrings.add)
+#     {        
+#         $tempconnectionNames = $tempconnectionNames + $setting.name
+#     }
+#     write-host "Finished Getting ConnectionString Names"
+#     return $tempconnectionNames
+# }
 
-function GetConnectionType ([string]$settingName) {
-            if ($UseSQLServerType -eq "true") {
-            if ($SQLServerConnectionNames.split(";").split(",").contains($settingName)) {
-                $connectionType = "SqlServer"
-            }
-        }
-        elseif ($UseAzureSQLType -eq "true") {
-            if ($AzureSQLConnectionNames.split(";").split(",").contains($settingName)) {
-                $connectionType = "AzureSQL"
-            }            
+# function GetConnectionType ([string]$settingName) {
+#             if ($UseSQLServerType -eq "true") {
+#             if ($SQLServerConnectionNames.split(";").split(",").contains($settingName)) {
+#                 $connectionType = "SqlServer"
+#             }
+#         }
+#         elseif ($UseAzureSQLType -eq "true") {
+#             if ($AzureSQLConnectionNames.split(";").split(",").contains($settingName)) {
+#                 $connectionType = "AzureSQL"
+#             }            
             
-        }
-        elseif ($UseMySqlType -eq "true") {
-            if ($MySQLConnectionNames.split(";").split(",").contains($settingName)) {
-                $connectionType = "MySql"
-            }
+#         }
+#         elseif ($UseMySqlType -eq "true") {
+#             if ($MySQLConnectionNames.split(";").split(",").contains($settingName)) {
+#                 $connectionType = "MySql"
+#             }
             
-        }
-        elseif ($UseCustomType -eq "true") {
-            if ($CustomTypeConnectionNames.split(";").split(",").contains($settingName)) {
-                $connectionType = "Custom"
-            }            
+#         }
+#         elseif ($UseCustomType -eq "true") {
+#             if ($CustomTypeConnectionNames.split(";").split(",").contains($settingName)) {
+#                 $connectionType = "Custom"
+#             }            
             
-        }
+#         }
 
-        $connectionType
-}
+#         $connectionType
+# }
 
 
 try {
@@ -139,6 +139,33 @@ try {
     [string]$UseCustomType = Get-VstsInput -Name CustomType
     [string]$CustomTypeConnectionNames = Get-VstsInput -Name CustomTypeConnectionNames
 
+    function GetConnectionType ([string]$settingName) {
+                if ($UseSQLServerType -eq "true") {
+                if ($SQLServerConnectionNames.split(";").split(",").contains($settingName)) {
+                    $connectionType = "SqlServer"
+                }
+            }
+            elseif ($UseAzureSQLType -eq "true") {
+                if ($AzureSQLConnectionNames.split(";").split(",").contains($settingName)) {
+                    $connectionType = "AzureSQL"
+                }            
+                
+            }
+            elseif ($UseMySqlType -eq "true") {
+                if ($MySQLConnectionNames.split(";").split(",").contains($settingName)) {
+                    $connectionType = "MySql"
+                }
+                
+            }
+            elseif ($UseCustomType -eq "true") {
+                if ($CustomTypeConnectionNames.split(";").split(",").contains($settingName)) {
+                    $connectionType = "Custom"
+                }            
+                
+            }
+
+            $connectionType
+    }
 
     Write-Host "Web App ame ->                  $WebAppName"
     Write-Host "Current Working directory ->    $cwd"
@@ -201,30 +228,43 @@ try {
     #     throw new-object System.ArgumentException
     # }
 
-    write-host "Starting Appsettings"
-    echo $configContent.configuration.appSettings.add
-    foreach($setting in $configContent.configuration.appSettings.add)
-    {
-        $appsettingsHash.Add($setting.key,$setting.value)
-        $appsettingsNames =$appsettingsNames + $setting.key
+    if ($UseAppsettings -eq "True") {
+
+        write-host "Starting Appsettings"
+        echo $configContent.configuration.appSettings.add
+        foreach($setting in $configContent.configuration.appSettings.add)
+        {
+            $appsettingsHash.Add($setting.key,$setting.value)
+            $appsettingsNames =$appsettingsNames + $setting.key
+        }
+
+        write-host "Finished Appsettings"
+        echo $appsettingsHash
     }
 
-    write-host "Finished Appsettings"
-    echo $appsettingsHash
+    if ($UseAppsettings -eq "true" -and $UseConnectionStrings -eq "true") {
+        write-host "*****************************"
+    }
+
     
-    write-host "*****************************"
 
-    write-host "Starting Conectionstrings"
-    echo $configContent.configuration.connectionStrings.add
-    foreach($setting in $configContent.configuration.connectionStrings.add)
-    {
-        $connectionType = GetConnectionType $setting.name
-        $connections[$setting.name] = @{Type = $connectionType;Value = $setting.connectionString}
-        $connectionNames = $connectionNames + $setting.name
+
+    if ($UseConnectionStrings -eq "true") {
+        write-host "Starting Conectionstrings"
+        echo $configContent.configuration.connectionStrings.add
+        foreach($setting in $configContent.configuration.connectionStrings.add)
+        {
+            $connectionType = GetConnectionType $setting.name
+            $connections[$setting.name] = @{Type = $connectionType;Value = $setting.connectionString}
+            $connectionNames = $connectionNames + $setting.name
+        }
+        write-host "Finished Parsing ConnectionString"
+        echo $connections
     }
-    write-host "Finished Parsing ConnectionString"
-    echo $connections
 
+    if ($UseAppsettings -eq "false" -and $UseConnectionStrings -eq "false") {
+        throw new-object System.ArgumentException
+    }
 
     Set-AzureRmWebAppSlotConfigName  -Name $WebAppName -ResourceGroupName $ResourceGroup -RemoveAllAppSettingNames -RemoveAllConnectionStringNames
 
